06645cc6329b3124ae31556ee238b164c0cf0658
Keshav Kumawat <keshavkumawat2005@gmail.com>
2026-06-13 21:39:09 +0530

    Clean up repository: remove unrelated project files

--- conclusion.py
+++
@@ -1,9 +0,0 @@
-import practice as np
-
-# Example: Creating a NumPy array
-arr = np.array([1, 2, 3, 4, 5])
-print(arr)
-
-# Example: Performing a mathematical operation
-squared_arr = arr ** 2
-print(squared_arr)
